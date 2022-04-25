60b5feddaf8164a2de88562181114303da47c3e0
Tessavdg <103995368+Tessavdg@users.noreply.github.com>
2022-04-25 11:06:09 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,16 @@ new fullpage('#fullpage', {
 	//options here
 	//autoScrolling:true,
 	//scrollHorizontally: true
+
+	anchors:['firstPage', 'secondPage', 'thirdPage']
 });
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
